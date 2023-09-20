5c44225ae5c948163c91c49528765b69da51bab4
satawatnack <satawat@bandprotocol.com>
2023-09-20 15:55:23 +0700

    add gmp

--- api/link_glibclinux_aarch64.go
+++ api/link_glibclinux_aarch64.go
@@ -2,5 +2,5 @@
 
 package api
 
-// #cgo LDFLAGS: -Wl,-rpath,${SRCDIR} -L${SRCDIR} -lgo_owasm.aarch64
+// #cgo LDFLAGS: -Wl,-rpath,${SRCDIR} -L${SRCDIR} -lgo_owasm.aarch64 -lgmp
 import "C"
--- api/link_glibclinux_x86_64.go
+++ api/link_glibclinux_x86_64.go
@@ -2,5 +2,5 @@
 
 package api
 
-// #cgo LDFLAGS: -Wl,-rpath,${SRCDIR} -L${SRCDIR} -lgo_owasm.x86_64
+// #cgo LDFLAGS: -Wl,-rpath,${SRCDIR} -L${SRCDIR} -lgo_owasm.x86_64 -lgmp
 import "C"
